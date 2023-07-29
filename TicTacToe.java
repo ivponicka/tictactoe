1c4493f16fcbe057709ee5912119322008df4921
ivponicka <56002673+ivponicka@users.noreply.github.com>
2023-07-29 15:11:45 +0200

    Adding board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class TicTacToe{
     public static void main(String[] args) {
         System.out.println("Welcome to TIC TAC TOE game!");
+        char[][] board = new char[3][3];
+
+
+    }
+
+    public static void showBoard(){
+
     }
 }
